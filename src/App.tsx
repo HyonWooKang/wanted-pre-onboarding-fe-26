a0e1d3654f410d5efd8f6450ac88ea08d82d712c
Hyon Woo Kang <87benzenes@gmail.com>
2024-10-04 02:44:25 +0900

    프로젝트 초기 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
+      <div className="App-body">
+        <div>
+          <h2>원티드 프리온보딩 FE 챌린지 10월 (2024)</h2>
+          <h4></h4>
+        </div>
+        <div className="table-group">
+          <div className="table-title">
+            <p></p>
+          </div>
+          <div className="table"></div>
+        </div>
+      </div>
     </div>
   );
 }
